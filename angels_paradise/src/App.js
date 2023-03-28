413d71c070d0632dd7725602f204386b8b77fb34
ASUS <abhiraskar@gmail.com>
2023-03-29 00:08:26 +0530

    mock

---
+++ angels_paradise/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Heading } from '@chakra-ui/react'
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Heading>ABHI's WORLD</Heading>
+    </div>
+  );
+}
+
+export default App;
